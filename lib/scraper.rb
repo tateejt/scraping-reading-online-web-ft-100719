18a857a5e10b03d388607cae76d58719fcccc7b6
Emery Tate <tateejt@gmail.com>
2019-10-27 00:57:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+flatiron = doc.css(".headline-260IBN").text
